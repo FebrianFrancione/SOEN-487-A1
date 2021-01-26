1eae6c0facbc7bd93111b2461ee65ce21e46bde1
LettuceFoot <febrian.francione@gmail.com>
2021-01-25 19:08:04 -0500

    Finished second GET request

--- AlbumAndArtists/src/main/java/com/example/AlbumAndArtists/AlbumRest.java
+++ AlbumAndArtists/src/main/java/com/example/AlbumAndArtists/AlbumRest.java
@@ -1,5 +1,7 @@
 package com.example.AlbumAndArtists;
 
+import org.glassfish.jersey.server.monitoring.ResponseMXBean;
+
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -11,43 +13,39 @@ import java.util.stream.Collectors;
 public class AlbumRest {
     private static ArrayList<Album> albums = new ArrayList<>();
     public static ArrayList<Album> arrayTest = new ArrayList<>();
-    ArrayList<Album> arrayTest2 = new ArrayList<>();
+    String message = "";
+
     //getting album by ISRC and title
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     @Path("/list")
     public Response getAlbum() {
+//        arrayTest.add(new Album("ISRC","title", "description", 1, "artist"));
+//        arrayTest.add(new Album("ISRC1","title1", "description1", 2, "artist1"));
+//        arrayTest.add(new Album("ISRC2","title2", "description2", 3, "artist2"));
 
-//       Album album  = arrayTest.stream().findFirst().orElse(null);
-        String message = "";
-        if(!arrayTest.isEmpty()){
-            for(int i = 0; i < arrayTest.size(); i++){
-                message += " ISRC: " + arrayTest.get(i).getISRC() + ", Title: " + arrayTest.get(i).getTitle() + "\n";
+        if(!albums.isEmpty()){
+            for(int i = 0; i < albums.size(); i++){
+                message += " ISRC: " + albums.get(i).getISRC() + ", Title: " + albums.get(i).getTitle() + "\n";
             }
             return Response.status(Response.Status.OK).entity(message).build();
         }else{
             message = "Error! No albums no return!";
             return Response.status(Response.Status.NOT_FOUND).entity(message).build();
         }
-
-//        return arrayTest.size();
-//       if(!arrayTest.isEmpty()){
-//           arrayTest.stream().forEach(b-> System.out.println(b.getISRC() + "" + b.getTitle()));
-//           return "got all";
-//       }else{
-//           return "Album not found!";
-//       }
     }
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     @Path("{ISRC}/{title}")
-    public String getAlbum(@PathParam("ISRC") String ISRC, @PathParam("title") String title) {
+    public Response getAlbum(@PathParam("ISRC") String ISRC, @PathParam("title") String title) {
         Album album = albums.stream().filter(album1 -> album1.getTitle().equals(title) && album1.getISRC().equals(ISRC)).findFirst().orElse(null);
         if (album != null) {
-            return album.toString();
+
+            return Response.status(Response.Status.OK).entity(album.toString()).build();
         }else{
-            return "Album not found!";
+            message = "Album not found!";
+            return Response.status(Response.Status.NOT_FOUND).entity(message).build();
         }
     }
 
