52e82400831dd00b36612e50a6d4c1c8a0c43a8e
ivan <garzon861@gmail.com>
2021-01-28 17:14:18 -0500

    Removed Artist and ArtistManager from Jersey module
    moved to demo module (Tomcat server)

--- AlbumAndArtists/src/main/java/TestMain.java
+++ AlbumAndArtists/src/main/java/TestMain.java
@@ -8,9 +8,9 @@ public class TestMain {
     public static void main(String[] args) {
 
         AlbumsManager albumsManager = new AlbumsManager();
-        ArtistsManager artistsManager = new ArtistsManager();
+        //ArtistsManager artistsManager = new ArtistsManager();
         ArrayList<Album> albums = new ArrayList<Album>();
-        ArrayList<Artist> artists = new ArrayList<Artist>();
+        //ArrayList<Artist> artists = new ArrayList<Artist>();
         int year = (int)(Math.random() * (2020 - 1979 + 1) + 1979);
 
         //Creating 10 random albums and artists
@@ -27,17 +27,19 @@ public class TestMain {
             Album newAlbum =new Album(ISRC, title, description, year, artist);
             albums.add(newAlbum);
             albumsManager.createAlbum(ISRC, title, description, year, artist);
-
+/*
             Artist newArtist = new Artist(nickname, first_name, last_name, biography);
             artists.add(newArtist);
             artistsManager.createArtist(nickname, first_name, last_name, biography);
+
+ */
 
             year = (int)(Math.random() * (2020 - 1979 + 1) + 1979);
         }
 
         //Printing all albums and artists
         System.out.println("\n\nGetting all albums\n" + albumsManager.getAllAlbums());
-        System.out.println("\n\nGetting all artists\n" + artistsManager.getAllArtists());
+        //System.out.println("\n\nGetting all artists\n" + artistsManager.getAllArtists());
 
         //Getting a specific album
         String isrc = albums.get(5).getISRC();
@@ -51,19 +53,21 @@ public class TestMain {
         //Deleting a specific album
         albumsManager.deleteAlbum(isrc);
         System.out.println("\n\nGetting all albums after deleting album with ISRC " + isrc + "\n" + albumsManager.getAllAlbums());
-
+/*
         //Getting a specific artist
         String nickname = artists.get(5).getNickname();
         Artist testArtist = artistsManager.getArtist(nickname);
         System.out.println("\n\nGetting artist with nickname : " + nickname + "\n" + testArtist);
 
-        //Updating a specific album
+        //Updating a specific artist
         if(artistsManager.updateArtist(nickname, "Cameron", "Diaz", "She's a sexy mamasita"))
             System.out.println("\n\nAfter modified artist " + nickname + ":\n" + artistsManager.getArtist(nickname));
 
-        //Deleting a specific album
+        //Deleting a specific artist
         artistsManager.deleteArtist(nickname);
         System.out.println("\n\nGetting all artists after deleting artist with nickname " + nickname + "\n" + artistsManager.getAllArtists());
+
+ */
     }
 
     //Helper to get some random words
